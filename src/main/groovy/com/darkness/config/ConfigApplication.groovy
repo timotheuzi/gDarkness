e73e0962a7b10bdd5c91696808ab781dfee56785
timotheuzi <timotheuzi@yahoo.com>
2021-08-15 21:14:56 -0500

    updates

--- src/main/groovy/com/darkness/config/ConfigApplication.groovy
+++ src/main/groovy/com/darkness/config/ConfigApplication.groovy
@@ -1,16 +1,25 @@
-/*package com.darkness.config
+package com.darkness.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 import com.darkness.utils.DarknessUtils
 
 @Configuration
-class ConfigApplication<config> {
+class ConfigApplication {
+
+    @Autowired
+    DarknessUtils darknessUtils = new DarknessUtils()
 
     // ##################################################################################################
     // method Bean
     // ##################################################################################################
+    @Bean
+    DarknessUtils darkBean()
+    {
+        DarknessUtils darknessUtils = new DarknessUtils()
+        return darknessUtils
+    }
 
-
-}*/
+}
